7f1356100164f75ebcbfaee7bd19e1560f906dc7
Kavindu-J22 <kavindujayasinghe2002122@gmail.com>
2025-11-22 17:14:22 +0530

    up

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -268,10 +268,11 @@ const Layout = () => {
       <div className="flex">
         {/* Sidebar */}
         <div className={`
-          fixed lg:static inset-y-0 left-0 z-50 w-64 bg-white dark:bg-gray-800 
+          fixed lg:static inset-y-0 left-0 z-50 w-64
+          bg-white/95 dark:bg-gray-800/95 backdrop-blur-sm
           border-r border-gray-200 dark:border-gray-700 transform transition-transform duration-300 ease-in-out
           ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
-          mt-16 lg:mt-0
+          mt-16 lg:mt-0 glass-shine
         `}>
           <div className="p-6">
             <nav className="space-y-2">
